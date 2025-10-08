7a3497abbe6aade964a01ce86cab16b2f0de3f67
Charles Treatman <ctreatman@equinix.com>
2025-10-08 11:17:38 -0500

    chore(docs): disable cobra autogen tag to avoid changing docs every day

--- cmd/root.go
+++ cmd/root.go
@@ -32,9 +32,10 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "equinix",
-	Short: "Equinix CLI",
-	Long:  `Command-line interface for Equinix APIs`,
+	Use:               "equinix",
+	Short:             "Equinix CLI",
+	Long:              `Command-line interface for Equinix APIs`,
+	DisableAutoGenTag: true,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
